4cdfc781c1e99dbe9dcef1501cc1c1239ed48da2
spushkin <pushkin.s89@gmail.com>
2024-10-22 14:58:58 -0700

    Cleanup

--- Test/Pushkin_Siarhei_HW6_main.c
+++ Test/Pushkin_Siarhei_HW6_main.c
@@ -1,7 +1,6 @@
 /**************************************************************
 * Class::  CSC-415-01 Summer 2024
 * Name:: Siarhei Pushkin
-* Student ID:: 922907437
 * GitHub-Name:: spushkin
 * Project:: Assignment 6 – Coding A Device Driver
 *
